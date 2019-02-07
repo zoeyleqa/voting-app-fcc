783fdd92b79b11321039a5b0abc3e76aca36ebe7
zoeyleqa <qal@terpmail.umd.edu>
2019-02-07 01:21:38 -0500

    attempt to get it deployed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyparser = require("body-parser");
 
 var app = express();
 require('dotenv').load();
-require('dotenv').config({process.env.HOME + '/.env'});
+// require('dotenv').config({process.env.HOME + '/.env'});
 require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
